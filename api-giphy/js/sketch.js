f0e93d9be18664faf197ec7f5caf2cd824d5e966
Zihan Shen <shenz708@newschool.edu>
2018-11-06 23:52:04 -0500

    apigiphy01

---
+++ api-giphy/js/sketch.js
@@ -0,0 +1,32 @@
+var api = "http://api.giphy.com/v1/gifs/search?";
+//var api = "http://api.giphy.com/v1/stickers/search?";
+
+//var thing = "rainbow";
+
+var apiKey = "&api_key=dc6zaTOxFJmzC&q=";
+
+//var query = "&q=rainbow";
+
+function setup(){
+	noCanvas();
+	var button = select('#submit');
+	button.mousePressed(abcAsk); 
+
+	input = select('#keyword'); 
+}
+
+function abcAsk(){
+	var url = api + apiKey + input.value();
+ //var url = api + apiKey + query;
+
+	loadJSON(url, gotData);
+}
+
+
+function gotData(giphy){
+	for (var i = 0; i < giphy.data.length; i++){
+		createImg(giphy.data[i].images.original.url);
+	}
+}
+
+
